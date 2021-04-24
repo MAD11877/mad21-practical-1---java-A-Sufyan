6d874ce0e0c8220fbe1aca731c0055ee38d2cd45
A-Sufyan <82924764+A-Sufyan@users.noreply.github.com>
2021-04-24 14:49:27 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,6 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter an integer: ");
     int number = in.nextInt();
     int timesNumber = number * number;
     System.out.println(timesNumber);
